12531a41049ff49522d5712b1f7513853c12ef40
HousniEL <elaich.housni@gmail.com>
2022-02-06 11:43:57 +0100

    server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static("dist/backoffice"));
+app.use(express.static(__dirname + "/dist/angularspring"));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/backoffice" });
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/dist/dictionary/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8098);
+app.listen(process.env.PORT || 8090);
